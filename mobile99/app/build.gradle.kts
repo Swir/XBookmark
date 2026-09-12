d42bd27d08ef365b50e204f0ab8fa73d84afe7f2
Jan Szczerbaty Uśmiechnięty <swirjestem@hotmail.com>
2026-09-12 12:47:34 +0200

    Bump 3+3 launcher test to 0.7.1-dev

--- mobile99/app/build.gradle.kts
+++ mobile99/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "pl.swir.czateria"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.7.0-dev"
+        versionCode = 11
+        versionName = "0.7.1-dev"
     }
 
     buildTypes {
